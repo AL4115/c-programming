b2c035f750ecc7f6297e9e01e3b1d5ff1add6920
AVANI-11-LODHI <avanilodhi4116@gamil.com>
2026-01-30 20:23:43 +0530

    array

---
+++ printreverse.c
@@ -0,0 +1,14 @@
+// wap to print array in reverse order
+# include<stdio.h>
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+    int x=i+1;
+    printf("enter the element number:%d\n",x);
+    scanf("%d",&arr[i]);
+    }
+    for(int i=4;i>=0;i--){
+        printf(" %d ",arr[i]);
+    }
+    return 0;
+}
