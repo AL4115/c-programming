f88fa81189c1e4f42140b186afdacf90bd3f44a3
AVANI-11-LODHI <avanilodhi4116@gamil.com>
2026-02-04 19:51:12 +0530

    array

---
+++ twopointertech.c
@@ -0,0 +1,24 @@
+// wap to find the targeted value by summation of pairs (two pointer technique)
+# include<stdio.h>
+int main (){
+   /* int arr[8]={1,2,3,5,7,10,11,15};
+    int current_sum;
+    int left;
+    int right;
+    while(left)
+    }
+    return 0;
+}*/
+
+// wap to find the address of an element given in the array
+int arr[6]={2,3,4,7,9,1};
+int BA = arr[0];
+int a;
+printf("enter the element no. a");
+scanf("%d",&a);
+int *ptr;
+ptr=&a;
+printf("%d",ptr);
+return 0;
+}
+
