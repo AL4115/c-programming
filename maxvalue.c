9c2b70833d0946b5881f48a0a15402a215049d33
AVANI-11-LODHI <avanilodhi4116@gamil.com>
2026-02-07 23:54:38 +0530

    array

---
+++ maxvalue.c
@@ -0,0 +1,28 @@
+// wap to print the maximum value out of all element of an array
+# include<stdio.h>
+int main(){
+   /* int arr[5] = {50,82,92,71,11};
+    int max= arr[0];
+    for(int i=0;i<5;i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
+        else{
+        }
+    }
+        printf(" %d ",max);
+    return 0;
+}*/
+// wap for minimum value 
+int arr[5]={50,82,92,71,11};
+int min=arr[0];
+for(int i=0;i<5;i++){
+    if(min>arr[i]){
+        min=arr[i];
+    }
+    else{
+    }
+}
+        printf(" %d ",min);
+        return 0;
+}
